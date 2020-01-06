b727090afb5765ec0ff2aeddffb33cfb5a58deeb
Irina <irina060981@yandex.ru>
2020-01-06 21:30:20 +1000

    update tests

--- tests/new-paradigm.test.js
+++ tests/new-paradigm.test.js
@@ -4,12 +4,16 @@ import { Constants, Feature, LanguageModelFactory } from 'alpheios-data-models'
 
 import BaseTestHelp from '@tests/data/base-test-help.js'
 
+import ViewSetFactory from '@views/lib/view-set-factory.js'
+
 import LanguageDatasetFactory from '@lib/language-dataset-factory.js'
 import GreekViewSet from '@views/lang/greek/greek-view-set.js'
 import LatinViewSet from '@views/lang/latin/latin-view-set.js'
 
 import GreekNounView from '@views/lang/greek/noun/greek-noun-view.js'
 import GreekVerbParadigmView from '@/paradigm/views/greek/verb/greek-verb-paradigm-view.js'
+
+import GreekParadigmDataset from '@/paradigm/data/greek/greek-paradigm-dataset.js'
 
 describe('greek-verb-paradigm.test.js', () => {
   // console.error = function () {}
@@ -31,75 +35,40 @@ describe('greek-verb-paradigm.test.js', () => {
   afterAll(() => {
     jest.clearAllMocks()
   })
-/*
-  it('1 - checked Verb Paradigm1 - βουλεύῃς', async () => {
-    const inflectionsViewSet = await BaseTestHelp.getInflectionSet('βουλεύῃς', Constants.LANG_GREEK)
 
-    expect(inflectionsViewSet.hasMatchingViews).toBeTruthy()
-    expect(inflectionsViewSet.matchingViews.length).toEqual(1)
+  it('1 - check paradigm (paradigm1) - βουλεύῃς', async () => {
+    const locale = "en-US"
+    let homonym = await BaseTestHelp.getHomonym('βουλεύῃς', Constants.LANG_GREEK)
+    const inflectionsViewSet = ViewSetFactory.create(homonym, locale)
+    const partOfSpeech = new Feature(Feature.types.part, Constants.POFS_VERB, Constants.LANG_GREEK)
 
-    // console.info('inflectionsViewSet.matchingViews - ', inflectionsViewSet.matchingViews[0])
-    BaseTestHelp.checkParadigm({
-      view: inflectionsViewSet.matchingViews[0],
-      viewName: 'GreekVerbParadigmView',
-      viewTitle: 'ω-Verbs: Present System Active',
-      paradigmID: 'verbpdgm1',
-      hasSuppParadigms: true,
-      suppParadigms: [ 'verbpdgm54' ]
-    })
+    let matchingViews = inflectionsViewSet.getViews(partOfSpeech.value)
+    expect(matchingViews.length).toEqual(1)
+
+    expect(matchingViews[0].constructor.name).toEqual('GreekVerbParadigmView')
+    expect(matchingViews[0].paradigm.paradigmID).toEqual('verbpdgm1')
   })
-*/
-  it.skip('2 - GPD', async () => {
-    // stage 1 ViewSetFactory.create
-    //  -- new GreekViewSet()
-    //  ---- this.dataset = LanguageDatasetFactory.getDataset(homonym.languageID)
-    // -----------------------
-    // const instance = this.instance
-    // if (instance.sets.has(languageID)) {
-    //   let dataset = instance.sets.get(languageID) // eslint-disable-line prefer-const
-    //   if (!dataset.dataLoaded) {
-    //     dataset.loadData()
-    //   }
-    //   return dataset
-    // } 
-    // -----------------------
-    // const languageID = Constants.LANG_GREEK
-    // const instance = LanguageDatasetFactory.instance
-    
-    // if (instance.sets.has(languageID)) {
-    //   instance.sets.get(languageID).forEach(dataset => {
-    //     if (!dataset.dataLoaded) {
-    //       dataset.loadData()
-    //     }
-    //   })
-    // }
 
-    // console.info('instance - ', instance.sets.get(languageID))
-    let words = ['οἱ', 'ἴτων', 'ἀγάγοις']
-    let homonym = await BaseTestHelp.getHomonym('iam', Constants.LANG_LATIN)
-    let datasets = LanguageDatasetFactory.getDatasets(Constants.LANG_LATIN)
-    console.info(datasets)
+  it('2 - check paradigm (paradigm1) - βουλεύῃς', async () => {
+    let homonym = await BaseTestHelp.getHomonym('βουλεύῃς', Constants.LANG_GREEK)
     
+    let dataset = new GreekParadigmDataset()
+    dataset.loadData()
+
     for (const lexeme of homonym.lexemes) {
-      for (let inflection of lexeme.inflections) {
-        console.info('inflection', inflection.constraints)
-        datasets.forEach(dataset => {
-          dataset.setInflectionData(inflection, lexeme.lemma)
-          console.info('inflection', dataset.constructor.name, ' - ', inflection.constraints)
-        })
-        
+      for (const inflection of lexeme.inflections) {
+        dataset.setInflectionData(inflection, lexeme.lemma)
       }
     }
 
+    const views = GreekViewSet.views
     let matchingViews = []
-    let views = LatinViewSet.views
-
-    // console.info('views', views)
-    
     matchingViews.push(...views.reduce(
       (acc, view) => acc.concat(...view.getMatchingInstances(homonym)), []))
-    
-    // console.info('matchingViews', matchingViews)
-    // console.info('matchingViews', matchingViews.map(view => view.constructor.name + ' - ' + (view.paradigm ? view.paradigm.paradigmID : '')))
+
+    expect(matchingViews.length).toEqual(1)
+
+    expect(matchingViews[0].constructor.name).toEqual('GreekVerbParadigmView')
+    expect(matchingViews[0].paradigm.paradigmID).toEqual('verbpdgm1')
   })
 })
